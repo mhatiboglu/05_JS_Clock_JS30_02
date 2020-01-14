4a2dc67f999ee10be6fbf3794b546778f751bfbf
mhatiboglu <mh1741mh@gmail.com>
2020-01-14 20:15:13 +0200

    css attributes and js functions added.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # 05_JS_Clock_JS30_02
+
+			setInterval()
+			
+The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).
+
+The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+
+
+setInterval(function(){ alert("Hello"); }, 3000);
+
+// Alert "Hello" every 3 seconds (3000 milliseconds):
+
+			setTimeout()
+			
+The setTimeout() method calls a function or evaluates an expression after a specified number of milliseconds.
+
+Tip: 1000 ms = 1 second.
+
+Tip: The function is only executed once. If you need to repeat execution, use the setInterval() method.
+
+Tip: Use the clearTimeout() method to prevent the function from running.
+
+
+setTimeout(function(){ alert("Hello"); }, 3000);
+
+//Display an alert box after 3 seconds (3000 milliseconds):
+
+
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//JS 
+//Second hand DOM
+const secondHand = document.querySelector(".second-hand");
+//Minute hand DOM
+const minsHand = document.querySelector(".min-hand");
+//Hour hand DOM
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  //We added 90 because of we declared as ' transform: rotate(90deg)' in css file
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+//Every 1000 miliseconds (1 second) we call setDate function
+setInterval(setDate, 1000);
+
+setDate();
